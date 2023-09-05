d7acb9064d8ad455ae2a7ca85dcdc6d2c9c46b6d
HstrPrn <socrew112@gmail.com>
2023-09-05 03:04:17 +0300

    Add api app

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,5 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from api.urls import urlpatterns
 
 
 admin.site.site_header = 'Foodgram'
@@ -7,4 +11,11 @@ admin.site.site_title = 'Foodgram'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(urlpatterns))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
