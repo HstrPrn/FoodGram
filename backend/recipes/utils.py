2ea23a6ccc9ae309520bbe6577aa6a3f34adab06
HstrPrn <socrew112@gmail.com>
2023-09-19 12:37:27 +0300

    some fixes

--- backend/recipes/utils.py
+++ backend/recipes/utils.py
@@ -7,11 +7,12 @@ from django.http import HttpResponse
 
 def download_csv(data: List[dict]) -> HttpResponse:
     response: HttpResponse = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = (
+        'attachment; filename="shopping_list.csv"'
+    )
+    response.write(u'\ufeff'.encode('utf8'))
     field_name: KeysView = data[0].keys()
     writer: csv.DictWriter = csv.DictWriter(response, field_name)
     for d in data:
         writer.writerow(d)
-    response['Content-Disposition'] = (
-        'attachment; filename="shopping_list.csv"'
-    )
     return response
