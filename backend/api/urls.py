3dbb7c0ea4ab00f39d5f5ee25f6e416ce0b6bcca
HstrPrn <socrew112@gmail.com>
2023-09-19 12:38:17 +0300

    Change project structure

--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-
-from recipes.urls import recipe_url_patterns
-from users.urls import users_urlpatterns
-
-urlpatterns = [
-    path('', include(recipe_url_patterns)),
-    path('', include(users_urlpatterns)),
-]
--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -2,16 +2,28 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from api.urls import urlpatterns as api_urls
+from recipes.views import TagViewSet, IngredientViewSet, RecipeViewSet
+from users.views import UserViewSet
 
 
 admin.site.site_header = 'Foodgram'
 admin.site.site_title = 'Foodgram'
 
+router = DefaultRouter()
+router.register('tags', TagViewSet, basename='tags')
+router.register(
+    'ingredients', IngredientViewSet, basename='ingredients'
+)
+router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('users', UserViewSet, basename='users')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(api_urls))
+    path('api/', include(router.urls)),
+    path('api/auth/', include('djoser.urls.authtoken')),
 ]
 
 if settings.DEBUG:
