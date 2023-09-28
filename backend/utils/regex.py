649922307dd9c8767d056d3e4bbab89564e4434f
HstrPrn <socrew112@gmail.com>
2023-09-28 04:10:33 +0300

    review #1 fixes

--- backend/utils/regex.py
+++ backend/utils/regex.py
@@ -1,4 +1,3 @@
-import string
 HEX_COLOR_REGEX: str = r'^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$'
 USERNAME_REGEX: str = r'^[\w.@+-]+\Z'
-TAG_NAME_REGEX: str = r'^[а-яА-Я]+[^]'
+TAG_NAME_REGEX: str = r'^[а-яА-Я]+'
