ea55dc641747a7a871079175a71966463493bc20
Ivan Efremov <122797868+HstrPrn@users.noreply.github.com>
2023-09-28 20:05:59 +0300

    Hstr dev (#11)
    * Work with models
    
    * Install gunicorn
    
    * Fix models
    
    * Add createingridient command
    
    * Some changes
    
    * Add User app with User and Follow models
    
    * Add admin site header and title
    
    * Some fixes
    
    * Some fixes
    
    * Add password validation
    
    * Some fixes
    
    * Add purchase_list model and some fixes
    
    * some fixes
    
    * Some fixes
    
    * Fix validator
    
    * Create api app
    
    * create api app
    
    * Add api app
    
    * Add tag and ingredient api
    
    * Some fixes
    
    * Fix .gitignore
    
    * Fix .gitignore
    
    * test .gitkeep
    
    * test
    
    * test .gitkeep v2
    
    * test
    
    * test
    
    * test
    
    * Del media
    
    * some fixes
    
    * Some fixes
    
    * Some fixes
    
    * Install drf, djoser
    
    * Some fixes
    
    * Add recipe api (incomplite)
    
    * Some fixes
    
    * Added recipe api with favorites and complete create and update
    
    * Added recipe api with favorites and complete create and update
    
    * Add user serializer
    
    * Some fixes
    
    * Some fixes
    
    * Add Follow, Cart serializer, view and some fixes
    
    * Some fixes
    
    * Add recipe filtration, download shopping list and some fixes
    
    * add custom paginator and some fixes
    
    * some fixes
    
    * adjust user api
    
    * add custom pagination
    
    * some fixes
    
    * some fixes
    
    * some fixes
    
    * some fixes
    
    * some fixes
    
    * Change project structure
    
    * some fixes
    
    * Add debug toolbar and some fixes
    
    * some fixes
    
    * some fixes
    
    * Add follow endpoints
    
    * Add docker files
    
    * Adjust nginx.conf
    
    * Add workflow
    
    * PR from master (#4)
    
    * test (#1)
    
    * Test workflow (#2)
    
    * test
    
    * test
    
    * test
    
    * test
    
    * Add PEP8 test
    
    * Add PEP8 test
    
    * Test
    
    * PEP8 test
    
    * Test
    
    * some fixes
    
    * some fixes
    
    * some fixes
    
    * some fixes
    
    * Push Docker Image to Docker Hub
    
    * Push Docker image to DockerHub
    
    * Push Docker image to DockerHub
    
    * Push Docker image to DockerHub
    
    * Push Docker image to DockerHub
    
    * some fixes
    
    * some fixes
    
    * some fixes
    
    * Add import tags and some fixes
    
    * add on push workflow
    
    * some fixes
    
    * Add deploy job
    
    * Test deploy step
    
    * Test deploy
    
    * Test deploy
    
    * Test deploy
    
    * Test deplloy
    
    * Test deploy
    
    * some fixes
    
    * Test deploy
    
    * Test deploy
    
    * some fixes
    
    * sofe mixes
    
    * some fixes
    
    * some fixes
    
    * some fixes
    
    * some fixzes
    
    * some fixes
    
    * some fixes
    
    * some fixes
    
    * some fixes
    
    * some fixws
    
    * some fixes
    
    * fix
    
    * some fixes
    
    * some fixes
    
    * some fixes
    
    * some fixes
    
    * some fixes
    
    * some fixes
    
    * add nginx dockerfile
    
    * fix csrf setting
    
    * PEP8
    
    * some fixes
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * add migration
    
    * some fixes
    
    * test
    
    * some fixes
    
    * fix
    
    * some fixes
    
    * fix
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * zaebal etot docker uje
    
    * mb its over
    
    * mb now
    
    * im so tired
    
    * update readme
    
    * update workflow
    
    * update workflow
    
    * some fixes
    
    * fix
    
    * fix
    
    * Review@v1
    
    * Update docker files
    
    * update reqs
    
    * some fixes
    
    * some fixes
    
    * review #1 fixes
    
    * review #1 fixes
    
    * review #1 fixes
    
    * review #1 fixes
    
    * review #1 fixes
    
    * some fixes
    
    * review #1 fixes
    
    * reviev #1 fixes
    
    ---------
    
    Co-authored-by: HstrPrn <socrew113@gmail.com>

--- backend/utils/regex.py
+++ backend/utils/regex.py
@@ -1,3 +1,3 @@
 HEX_COLOR_REGEX: str = r'^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$'
 USERNAME_REGEX: str = r'^[\w.@+-]+\Z'
-TAG_NAME_REGEX: str = r'^[а-яА-Я]+'
+TAG_NAME_REGEX: str = r'([а-яА-ЯёЁ0-9]\s[а-яА-ЯёЁ0-9]|[а-яА-ЯёЁ0-9])'
